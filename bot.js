4d0da125364a6af3048fc0d51572f6ec8366bc9c
reiyua <rei@reiyua.lol>
2024-01-22 08:11:22 +0000

    What the fuck am I thinking? I dont need discord.js for this

--- bot.js
+++
@@ -1,8 +0,0 @@
-// import required modules
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const config = require('./config.json');
-const fs = require('fs');
-const prefix = config.prefix;
-const token = config.token;
-const ownerID = config.ownerID;
